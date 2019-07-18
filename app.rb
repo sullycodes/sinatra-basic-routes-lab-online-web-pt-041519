b3077c41e1ef2ae62bb941251e31f4cf14e5981d
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-18 18:18:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end
+  
+  get '/name' do 
+    "My name is "
+    status = 200
+  end
+  
+  get '/hometown' do 
+    "My hometown is "
+  end
+  
+  # get '/' do 
+  #   "Hello, World!"
+  # end
+  
 end
